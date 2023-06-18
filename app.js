a020cd2761cf98a6377a54378161f8c944fb769d
Omar Alshafei <omaralshafei02@gmail.com>
2023-06-18 19:37:11 -0400

    made .env file

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var button = document.querySelector('.submit');
-var myApi = config.MY_API_KEY;
 var myApi = process.env.MY_API_KEY;
 button.addEventListener('click', function() {
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=' + myApi)
